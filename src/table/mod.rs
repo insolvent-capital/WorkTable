154b06861c3e7c4319f507948e68359ca0108c5a
Handy-caT <37216852+Handy-caT@users.noreply.github.com>
2024-09-13 16:58:11 +0300

    add more array examples

--- src/table/mod.rs
+++ src/table/mod.rs
@@ -150,7 +150,6 @@ mod tests {
 
         use crate::prelude::*;
         use crate::primary_key::TablePrimaryKey;
-        use crate::table::tests::enum_::SomeEnum;
 
         type Arr = [u32; 4];
 
@@ -197,6 +196,25 @@ mod tests {
             let selected_row = table.select(pk).unwrap();
             assert_eq!(selected_row, new_row);
             assert!(table.select(2.into()).is_none())
+        }
+
+        #[tokio::test]
+        async fn update_in_a_middle() {
+            let table = TestWorkTable::default();
+            for i in 0..10 {
+                let row = TestRow {
+                    id: i,
+                    test: [0; 4],
+                };
+                let _ = table.insert::<{ TestRow::ROW_SIZE }>(row.clone()).unwrap();
+            }
+            let new_row = TestRow {
+                id: 3,
+                test: [1; 4]
+            };
+            table.update::<{ TestRow::ROW_SIZE }>(new_row.clone()).await.unwrap();
+            let selected_row = table.select(3.into()).unwrap();
+            assert_eq!(selected_row, new_row);
         }
     }
 
